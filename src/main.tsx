2e587f1bb963c043c5cf54cc270f944834f2949e
Mark Anthony Estopace <estopace.ma@gmail.com>
2022-01-24 23:47:27 +0800

    setup tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { App } from "./App";
 import { createGlobalStyle } from "styled-components";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
 const GlobalStyle = createGlobalStyle`
   body {
