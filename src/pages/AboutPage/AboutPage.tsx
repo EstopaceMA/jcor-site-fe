748d43d2688d4a3eb73d80efbc585f2c13173e9a
Jabez Borja <jabez.natsu@gmail.com>
2022-12-22 01:05:50 +0800

    Fix: About Mobile Responsibility

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -4,21 +4,21 @@ export const AboutPage = () => {
   return (
     <div>
       <section className="h-screen flex flex-col justify-center items-center">
-        <div className="text-9xl font-bold -space-y-10">
+        <div className="text-5xl md:text-9xl font-bold -space-y-3 md:-space-y-10">
           <p>EVERYONE</p>
           <p className="ml-4">EVERYWHERE</p>
           <p>EVERYDAY</p>
         </div>
       </section>
 
-      <p className="text-center text-3xl font-bold">Empowering people to use what God has given them to serve and love our world through relational disciple-making.</p>
+      <p className="mx-5 md:mx-0 text-center text-3xl font-bold">Empowering people to use what God has given them to serve and love our world through relational disciple-making.</p>
 
       <section className="mt-32">
-        <div className="flex flex-row justify-evenly">
+        <div className="flex flex-col md:flex-row items-center md:items-start md:justify-evenly">
 
-          <img className="w-5/12" src="src/assets/images/pastor_lheo.jpg"/>
+          <img className="w-11/12 md:w-5/12" src="src/assets/images/pastor_lheo.jpg"/>
 
-          <div className="mt-10 w-6/12 text-right">
+          <div className="mt-10 w-11/12 md:w-6/12 text-center md:text-right">
             <p className="text-7xl font-bold">Meet our Lead Pastor</p>
             <p className="mt-2 text-xl">Pastor Lheo Bautista.. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tellus orci, faucibus ut tempor semper, suscipit nec orci. Donec in lorem dui. In gravida lectus vel tempus eleifend. Sed dignissim sapien lectus, vehicula viverra libero tincidunt eget. Aliquam placerat porta justo eget elementum. Fusce vulputate eget arcu sed volutpat. Mauris iaculis rhoncus finibus. Nulla facilisi. Fusce aliquam libero ut sapien laoreet, et blandit dui facilisis. Fusce sit amet urna sit amet ex pharetra luctus. Morbi ultrices, sem at elementum mattis, nisi est ullamcorper nisl, vitae laoreet ex nisi at massa. Integer fermentum est nisl, ut pulvinar felis euismod ac.</p>
           </div>
